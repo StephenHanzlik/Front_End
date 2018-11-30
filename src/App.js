d375bf3e3cb32d4db50bf6dc813ea69a33690f49
stephenhanzlik <steve@cloud-elements.com>
2018-11-30 07:06:10 -0700

    have basic parallax and modal on page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Button, Icon, Modal, Parallax} from 'react-materialize'
 
 // https://react-materialize.github.io/#/
 
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +22,23 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <div>
+          <Parallax imageSrc={require('./images/landing-page-img.jpg')}/>
+            <div className="section white">
+              <div className="row container">
+                <h2 className="header">Parallax</h2>
+                <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+              </div>
+            </div>
+          <Parallax imageSrc="http://materializecss.com/images/parallax2.jpg"/>
+        </div>
+        <Modal
+          header='Modal Header'
+          trigger={<Button waves='light'>OR ME!<Icon right>insert_chart</Icon></Button>}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.</p>
+        </Modal>
       </div>
     );
   }

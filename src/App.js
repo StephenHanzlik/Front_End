f68129405df227a9b566bf5464612fe3cf45bc90
stephenhanzlik <steve@cloud-elements.com>
2018-11-30 06:49:10 -0700

    have semantic ui cdns in and got dev server running again

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// https://react-materialize.github.io/#/
+
 class App extends Component {
   render() {
     return (

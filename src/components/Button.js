9708c4da6051a4c7f68954bf62a6c83a1d95cab3
StephanHanzlik <stephenhanzlik@gmail.com>
2019-12-12 19:04:45 -0700

    Started generic button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const ButtonTest = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
-
+const GenericButton = styled.button`
+  color: #ffb74d;
+  back-ground: black;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid #ffb74d;
   border-radius: 3px;
+  background: none;
 `;
 
 class Button extends Component {
     render(){
         return (
-            <ButtonTest/>
+            <GenericButton>SNOTEL Console</GenericButton>
         )
     }
 }

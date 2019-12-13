8a46716709a55341c7f3bbe418f54aebfc9b6690
StephanHanzlik <stephenhanzlik@gmail.com>
2019-12-12 18:13:33 -0700

    Have landing broke into div and styled in file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
 import backGroundImg from './images/landing-page-img.jpg';
-import skisImage from './images/skis-and-sticks.png';
- 
+import LandingHeader from './components/LandingHeader';
 
 const Background = styled.div`
     background-image: url(${backGroundImg});
@@ -12,21 +11,18 @@ const Background = styled.div`
     height: 100vh;
 `;
 // currently handled in CSS.  Should impliment with styled components
-// const Header = styled.div`
-//     margin-top: -25%;
-// `;
+const LandingHeaderContainer = styled.div`
+    margin-top: -44%;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Background/>
-        <div> 
-          <h1 id="header-1" className="Header center orange-text text-lighten-2">SUNSHINE <img src={skisImage} alt="skis" id="subHead"></img> DAYDREAM</h1>
-           <div className="row center">
-              <h5 className="header col s12 orange-text text-lighten-2">a backcountry snow and weather app</h5>
-           </div>
-        </div> 
+        <LandingHeaderContainer>
+            <LandingHeader/>
+        </LandingHeaderContainer>
       </div>
     );
   }
---
+++ src/components/LandingHeader.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../App.css';
+import styled from 'styled-components';
+import skisImage from '../images/skis-and-sticks.png';
+
+class LandingHeader extends Component {
+    render(){
+        return (
+        <div> 
+            <h1 className="Header center orange-text text-lighten-2">SUNSHINE <img src={skisImage} alt="skis" id="subHead"></img> DAYDREAM</h1>
+            <div className="row center">
+                <h5 className="header col s12 orange-text text-lighten-2">a backcountry snow and weather app</h5>
+             </div>
+        </div> 
+        )
+    }
+}
+
+export default LandingHeader

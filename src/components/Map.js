d0fb164132de3e55970b76bf90398c3c829a6a4d
StephanHanzlik <stephenhanzlik@gmail.com>
2019-12-14 15:47:12 -0700

    Have map added and centered

---
+++ src/components/Map.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+const MapBox = ReactMapboxGl({
+    accessToken:
+      'pk.eyJ1Ijoic3RlcGhlbmhhbnpsaWsiLCJhIjoiY2o1d3l5ZGhqMDJ3azJ3cXFxc2FsYjQzcyJ9.nvGZW-k1vzXxmWxmadEPmw'
+  });
+
+class Map extends Component {
+    render(){
+        return(
+            <MapBox
+                style="mapbox://styles/mapbox/outdoors-v11"
+                containerStyle={{
+                    width: '1200px',
+                    height: '60vh'
+                }}
+            >
+            <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+            </Layer>
+            </MapBox>
+        )
+    }
+}
+
+export default Map;
--- src/pages/Console.js
+++ src/pages/Console.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import NavBar from '../components/NavBar';
 import PrimaryGraph from '../components/PrimaryGraph';
+import Map from '../components/Map';
+import styled from 'styled-components';
+
+const MapWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
 
 class Console extends Component{
-
     render(){
         return(
             // eventualy pass props to graph and allow for 
             // customization of the dashboard
             <div>
                 <NavBar/>
+                <MapWrapper>
+                    <Map/>
+                </MapWrapper>
                 <PrimaryGraph/>
             </div>
         )

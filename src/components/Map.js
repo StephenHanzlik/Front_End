3abb3d5fd8d7d2d7e608656651cda53ef1e1c6d7
StephanHanzlik <stephenhanzlik@gmail.com>
2020-05-06 11:47:09 -0600

    Have json point on a map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-// import mapboxgl from 'mapbox-gl';
+import ReactMapboxGl, { Layer, Feature, Source } from 'react-mapbox-gl';
 import axios from 'axios';
+import { svg } from '../images/test.svg';
 
 const MapBox = ReactMapboxGl({
     // This is a public sandbox token.  It should be hidden as environment variable once I get new token for production.
@@ -9,6 +9,37 @@ const MapBox = ReactMapboxGl({
       'pk.eyJ1Ijoic3RlcGhlbmhhbnpsaWsiLCJhIjoiY2o1d3l5ZGhqMDJ3azJ3cXFxc2FsYjQzcyJ9.nvGZW-k1vzXxmWxmadEPmw'
   });
 
+// // Define layout to use in Layer component
+// const layoutLayer = { 'icon-image': 'londonCycle' };
+
+
+// // Create an image for the Layer
+// const image = new Image();
+// image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa(svg);
+// const images = ['londonCycle', image];
+
+const POSITION_CIRCLE_PAINT = {
+    'circle-stroke-width': 4,
+    'circle-radius': 10,
+    'circle-blur': 0.15,
+    'circle-color': '#3770C6',
+    'circle-stroke-color': 'white'
+  };
+
+  const GEOJSON_SOURCE_OPTIONS = {
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-0.13235092163085938,51.518250335096376]
+      },
+      properties: {
+        title: 'Mapbox DC',
+        'marker-symbol': 'monument'
+      }
+    }
+  };
 
 class Map extends Component {
 
@@ -19,6 +50,27 @@ class Map extends Component {
         };
 
     }
+
+    convertToGeoJson(stations){
+    let geoJson = stations.map(station => {
+       let geoJsonObj = {
+            type: 'geojson',
+            data: {
+                   type: 'Feature',
+                geometry: {
+                    type: 'Point',
+                coordinates: station.location
+                },
+                   properties: {
+                    title: station.name,
+                    'marker-symbol': 'monument'
+                    }
+            }
+           }
+        return geoJsonObj
+    })
+    return geoJson;
+    };
 
     componentDidMount(){
 
@@ -43,16 +95,21 @@ class Map extends Component {
                     width: '1200px',
                     height: '60vh'
                 }}
-                images={{
-                    "pin": "https://www.pikpng.com/transpng/imhRhJx/"
-                }}
             >
-            <Layer type="symbol" id="marker" layout={{
-                'icon-image': 'marker-15'
-                // 'icon-image': 'pin'
-                }}>
-                <Feature coordinates={[-0.13235092163085938,51.518250335096376]} />
-            </Layer>
+            <div>
+              <Source id="example_id" geoJsonSource={GEOJSON_SOURCE_OPTIONS} />
+              <Layer
+                type="circle"
+                id="example_id_marker"
+                paint={POSITION_CIRCLE_PAINT}
+                sourceId={'example_id'}
+              />
+            </div>
+                <Layer type="symbol" id="marker" 
+                    paint={POSITION_CIRCLE_PAINT}
+                >
+                    <Feature coordinates={[-0.13235092163085938,51.518250335096376]} />
+                </Layer>
             </MapBox>
         )
     }

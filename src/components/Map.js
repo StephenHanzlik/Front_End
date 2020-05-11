51f178268fb2af742b9d21f97eb8fc1ce2f6c39d
StephanHanzlik <stephenhanzlik@gmail.com>
2020-05-10 20:20:12 -0600

    moved map load into function.  calling it after GET /stations call

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
 
+
+// https://docs.mapbox.com/help/tutorials/use-mapbox-gl-js-with-react/
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcGhlbmhhbnpsaWsiLCJhIjoiY2thMGNkNnhiMDF5aDNubWtmbDNybmpjaCJ9.DHmoxylArLlQyZ1elyfyCA';
 
 class Map extends Component {
@@ -13,6 +15,8 @@ class Map extends Component {
             lat: 34,
             zoom: 2
         };
+
+        this.loadMap = this.loadMap.bind(this);
     }
 
     convertToGeoJson(stations) {
@@ -29,16 +33,16 @@ class Map extends Component {
             coordinates.push(location.lat);
 
             let geoJsonItem = {
-                type: 'geojson',
-                data: {
-                    type: 'Feature',
-                    geometry: {
-                        type: 'Point',
-                        coordinates: coordinates
+                'type': 'geojson',
+                'data': {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': coordinates
                     },
-                    properties: {
-                        title: station.name,
-                        'marker-symbol': 'monument'
+                    'properties': {
+                        'title': station.name,
+                        'icon': 'monument'
                     }
                 }
             }
@@ -50,6 +54,18 @@ class Map extends Component {
 
     componentDidMount() {
 
+        axios.get('http://localhost:8081/EnosJava/api/snotel/stations')
+            .then(response => {
+                let tempGeoJson = this.convertToGeoJson(response.data);
+                this.setState({
+                    stationGeoJson: tempGeoJson,
+                    renderStations: true
+                }, this.loadMap())
+            })
+            .catch(error => console.log(error))
+    }
+
+    loadMap(){
         const map = new mapboxgl.Map({
             container: this.mapContainer,
             style: 'mapbox://styles/mapbox/streets-v11',
@@ -57,16 +73,66 @@ class Map extends Component {
             zoom: this.state.zoom
         });
 
+        map.on('move', () => {
+            this.setState({
+                lng: map.getCenter().lng.toFixed(4),
+                lat: map.getCenter().lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            });
+        });
 
-        axios.get('http://localhost:8081/EnosJava/api/snotel/stations')
-            .then(response => {
-                let tempGeoJson = this.convertToGeoJson(response.data);
-                this.setState({
-                    stationGeoJson: tempGeoJson,
-                    renderStations: true
-                })
-            })
-            .catch(error => console.log(error))
+        map.on('load', function () {
+            map.addSource('points', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': [
+                        {
+                            // feature for Mapbox DC
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [
+                                    -77.03238901390978,
+                                    38.913188059745586
+                                ]
+                            },
+                            'properties': {
+                                'title': 'Mapbox DC',
+                                'icon': 'monument'
+                            }
+                        },
+                        {
+                            // feature for Mapbox SF
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [-122.414, 37.776]
+                            },
+                            'properties': {
+                                'title': 'Mapbox SF',
+                                'icon': 'harbor'
+                            }
+                        }
+                    ]
+                }
+            });
+            map.addLayer({
+                'id': 'points',
+                'type': 'symbol',
+                'source': 'points',
+                'layout': {
+                    // get the icon name from the source's "icon" property
+                    // concatenate the name to get an icon from the style's sprite sheet
+                    'icon-image': ['concat', ['get', 'icon'], '-15'],
+                    // get the title name from the source's "title" property
+                    'text-field': ['get', 'title'],
+                    'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                    'text-offset': [0, 0.6],
+                    'text-anchor': 'top'
+                }
+            });
+        });
     }
 
     render() {
@@ -74,9 +140,12 @@ class Map extends Component {
         return (
 
             <div>
-                <div ref={el => this.mapContainer = el} className="mapContainer" />
-            </div> 
-      
+                <div>
+                    <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+                </div>
+                <div ref={el => this.mapContainer = el} className='mapContainer' />
+            </div>
+
         )
     }
 }

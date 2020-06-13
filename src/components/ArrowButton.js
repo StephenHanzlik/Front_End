32d74d4aa48eb03e4238b8fcae2c90e07e34443d
StephanHanzlik <stephenhanzlik@gmail.com>
2020-06-13 13:53:55 -0600

    fixed some row styling

--- src/components/ArrowButton.js
+++ src/components/ArrowButton.js
@@ -7,6 +7,7 @@ import rightArrowImg from '../images/right-arrow.png';
 const ButtonWrapper = styled.div`
     width: 28px;
     height: 28px;
+    margin: 12px 5px 10px 5px;
 `;
 
 const Img = styled.img`

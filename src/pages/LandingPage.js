6c791f0728185f6fe211d56c2b13c8cb3ef5eff4
StephanHanzlik <stephenhanzlik@gmail.com>
2020-10-17 17:19:15 -0600

    have polaroids and imgs set up

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import backGroundImg from '../images/landing-page-img.jpg';
 import snotelStation from '../images/snotelStation.jpg';
-import meteorBurst from '../images/meteorBurst.png';
+import meteorBurst from '../images/meteorBurst.jpg';
 import LandingHeader from '../components/LandingHeader';
 import Button from '../components/Button';
 import styled from 'styled-components';
@@ -33,8 +33,14 @@ const ButtonWrapper = styled.div`
     flex-direction: column;
 `;
 
+const ParagraphContainer = styled.div`
+    width: 65%;
+    margin-left: auto;
+    margin-right: auto;
+`
+
 const PolaroidContainer = styled.div`
-    width: 40%;
+    width: 32%;
     background-color: white;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     margin-bottom: 25px;
@@ -53,6 +59,16 @@ class LandingPage extends Component {
     // }
 
     render(){
+        const imgStyle = {
+            'width': '100%'
+        }
+
+        const paragraphStyle = {
+            'font-size': '22px',
+            'margin-top': '30px',
+            'margin-bottom': '30px'
+        }
+
         return(
         <div>
             <Background/>
@@ -72,18 +88,32 @@ class LandingPage extends Component {
                 </ButtonWrapper>
             </LandingHeaderContainer>
             <AboutContentContainer>
-                <h2>About SNOTEL</h2>
+                <h3>SNOTEL and Mountain Snowpack</h3>
+                <ParagraphContainer>
+                    <p style={paragraphStyle}>SNOTEL stations are automated snow and weather observation stations run by the Natural Resources Conservation Service (NRCS).  They are located in remote
+                    mountain areas and are powered by solar panels.  They are designed to operate unattended and without maintenance for a year.  The NRCS receives data
+                    from the stations via meteor burst communications technology to collect data in near real time.  Meteor burst technology effectivly bounces VHF radio signals
+                    off an ever-present band of iononized meteors 50 to 70 miles above the earths surface.  Because of this the entire system does not use any satellites.</p>
+                </ParagraphContainer>
                 <PolaroidContainer>
-                    <img src={snotelStation} alt='snotelStation' style={{'width':'100%'}}/>
+                    <img src={meteorBurst} alt='meteorBurst' style={imgStyle}/>
+                    <PolaroidDiscription>
+                        <p>Meteor burst technology</p>
+                    </PolaroidDiscription>
+                </PolaroidContainer>
+                <ParagraphContainer>
+                    <p style={paragraphStyle}>The Mountain Snowpack application uses APIs provided by the NRCS meant to deliver CSV files used in academic and government reports.  We convert these CSVs for on the fly graphing and station exlporation.
+                    Because of the nature of meteor burst technology, this app is not always real time and will appear as if current data is missing.  Check back in for the most up to date information or explore past data points to extrapolate current 
+                    conditions.  This application primarly focuses on Snow Depth, Snow Water Equivalent, Air temperuature, and the 24 hours changes in these values.  This data is of importance to backcountry skiers, climbers, hikers, and other 
+                    recreationalists who are interested in the current status of the snowpack.
+                    </p>
+                </ParagraphContainer>
+                <PolaroidContainer>
+                    <img src={snotelStation} alt='snotelStation' style={imgStyle}/>
                     <PolaroidDiscription>
                         <p>A typical SNOTEL station</p>
                     </PolaroidDiscription>
                 </PolaroidContainer>
-                <p>SNOTEL stations are automated snow and weather observation stations run by the Natrual Resources Conservation Service (NRCS).  They are located in remote
-                    mountain areas and are primarily powered by solar panels.  They are designed to operate unattended and without mantaince for a year.  The NRCS recieves data
-                    from the stations via meteor burst communications technology to collect data in near real time.  Meteor burst technology effectivly bounces VHF radio signals
-                    off an ever-present band of iononized meteors 50 to 70 miles above the earths surface.  Because of this the entire system does not use any satellites.</p>
-                <img src={meteorBurst} alt='meteorBurst'/>
             </AboutContentContainer>
         </div> 
         )

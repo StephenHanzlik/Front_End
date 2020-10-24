6c40911d1ab9c85c627dc548b4738161d427df17
StephanHanzlik <stephenhanzlik@gmail.com>
2020-10-23 22:29:35 -0600

    have select for time intervals

--- src/pages/Console.js
+++ src/pages/Console.js
@@ -29,10 +29,13 @@ class Console extends Component {
             geoJson: '',
             callMade: false,
             callReturned: false,
-            latanceyTimeout: false
+            latanceyTimeout: false,
+            absoluteTime: 5184000000
         };
         this.getObservations = this.getObservations.bind(this);
         this.getStations = this.getStations.bind(this);
+        this.handleAbsoluteTimeChange = this.handleAbsoluteTimeChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
@@ -54,7 +57,12 @@ class Console extends Component {
         //NRCS UI Report Gen
         //https://www.nrcs.usda.gov/wps/portal/wcc/home/quicklinks/imap#version=115&elements=D&networks=!&states=!&counties=!&hucs=&minElevation=&maxElevation=&elementSelectType=all&activeOnly=true&activeForecastPointsOnly=false&hucLabels=false&hucIdLabels=false&hucParameterLabels=false&stationLabels=&overlays=&hucOverlays=&basinOpacity=100&basinNoDataOpacity=100&basemapOpacity=100&maskOpacity=0&mode=stations&openSections=dataElement,parameter,date,basin,elements,location,networks&controlsOpen=true&popup=05K29:CO:SNOW&popupMulti=&base=esriNgwm&displayType=inventory&basinType=6&dataElement=SRVO&depth=-8&parameter=PCTAVG&frequency=MONTHLY&duration=custom&customDuration=1&dayPart=E&year=2020&month=4&day=19&monthPart=E&forecastPubMonth=5&forecastPubDay=1&forecastExceedance=50&seqColor=1&divColor=3&scaleType=D&scaleMin=&scaleMax=&referencePeriodType=POR&referenceBegin=1981&referenceEnd=2010&minimumYears=20&hucAssociations=true&lat=39.8167&lon=-105.6999&zoom=12.5
         let currentDate = Date.now();
-        let startDate = currentDate - 7776000000 //90 days
+        let startDate = currentDate - this.state.absoluteTime;
+        //Probably want a drop down for this 
+        //7776000000 - 90 days
+        //5184000000 - 60 days
+        //2592000000 - 30 days
+        //604800000 - 7 days
 
         currentDate = new Date(currentDate).toJSON().slice(0, 10)
         startDate = new Date(startDate).toJSON().slice(0, 10)
@@ -76,6 +84,7 @@ class Console extends Component {
         axios.get(`/api/snotel/observations/${triplet}?from=${startDate}&to=${currentDate}`)
             .then(response => {
                 this.setState({
+                    //This is called in from props and set to state after the call.  Should change this
                     stationTriplet: triplet,
                     observations: response.data,
                     callReturned: true,
@@ -99,6 +108,17 @@ class Console extends Component {
         return geoJsonFeatureCollection;
     };
 
+    handleAbsoluteTimeChange(event) { 
+        this.setState({
+            absoluteTime: event.target.value
+        })
+    }
+    
+    handleSubmit(event) {
+        event.preventDefault();
+        this.getObservations(this.state.stationTriplet);
+    }
+
     render() {
         const paragraphCenteredStyle = {
             'width': '45%',
@@ -114,6 +134,11 @@ class Console extends Component {
 
         const paragraphStyle = {
             'font-size': '20px'
+        }
+
+        const timeSelectStyle = {
+            'display': 'initial',
+            'max-width': '80px'
         }
 
         return (
@@ -136,10 +161,23 @@ class Console extends Component {
                     {/* TODO: better logic here.  Perhaps checking state with specific flags for what to show
                     or having the logic in other components. */}
                     {this.state.observations.length >= 1 && this.state.callReturned &&
+                    <div>
                         <Graph
                             graphType={'snowDepth'}
                             observations={this.state.observations}
                         />
+                        {/* This should probably be part of the graph component or its own componentif used on details page as well */}
+                        <form onSubmit={this.handleSubmit}>
+                            <select style={timeSelectStyle} name={'selectAbsoluteTimeInterval'} value={this.state.absoluteTime} onChange={this.handleAbsoluteTimeChange}>
+                                <option value="604800000">7 days</option>
+                                <option value="2592000000">30 days</option>
+                                <option value="5184000000">60 days</option>
+                                <option value="7776000000">90 days</option>
+                                <option value="31536000000">365 days</option>
+                            </select>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
                     }
                     {this.state.observations.length < 1 && this.state.stationTriplet === '' && !this.state.callMade && 
                         <p style={paragraphCenteredStyle}>Select a station icon <img src={snowFlake} style={snowFlakeStyle} alt='snowFlake'/> to see an overview of the stations 
